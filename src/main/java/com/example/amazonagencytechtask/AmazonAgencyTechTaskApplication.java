3e69f51d27846250e77e1f1d5cfa5431ed686d27
mIst <bogdankharchenko77@gmail.com>
2024-02-28 13:57:06 +0200

    project created

---
+++ src/main/java/com/example/amazonagencytechtask/AmazonAgencyTechTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.amazonagencytechtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonAgencyTechTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonAgencyTechTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/amazonagencytechtask/AmazonAgencyTechTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amazonagencytechtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonAgencyTechTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
